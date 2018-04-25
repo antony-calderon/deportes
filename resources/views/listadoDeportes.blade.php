af97e324cff7b9f72985a63cde3a6fc876dc9fd9
Unknown <alex_aact@hotmail.com>
2018-04-24 20:27:03 -0500

    tabla de datos
    se organiza la tabla de datos extraidos de la base de datos

--- resources/views/listadoDeportes.blade.php
+++ resources/views/listadoDeportes.blade.php
@@ -6,12 +6,50 @@
     	<H2>Listado de deportes</H2>
 
     	<ul>
-    		@foreach ($deportesvarios as $deportesvario)
-    			{{$deportesvario->id}}
-    			<li>{{$deportesvario->deporte}}</li>
-    			
+    		<table>
+				<tr>
+					<th>Id </th>
+					<th>deporte </th>
+					<th>padre  </th>
+					<th>descripcion  </th>
+				</tr>
+				<br>
+				@foreach ($deportesvarios as $deportesvario)
+				<tr>
+					<td>{{$deportesvario->id}}</td>
+					<td>{{$deportesvario->deporte}}</td>
+					<td>{{$deportesvario->padre}}</td>
+					<td>{{$deportesvario->descripcion}}</td>
+				</tr>
+				@endforeach
+			</table>
+    		<!--@foreach ($deportesvarios as $deportesvario)
+    		<div class="row">
+    			<div class="col-sm">
+                    <label class="text-light">Id</label>	
+                    {{$deportesvario->id}}             
+                </div>
+                <div class="col-sm">
+                    <label class="text-light">Deporte</label>	
+                    {{$deportesvario->deporte}}             
+                </div>
+                <div class="col-sm">
+                    <label class="text-light">Padre</label>
+                    {{$deportesvario->padre}}             
+                </div>
+                <div class="col-sm">
+                    <label class="text-light">Descripcion</label>
+                    {{$deportesvario->descripcion}}        
+                </div>
+            </div>
+            @endforeach-->
+    		<!--@foreach ($deportesvarios as $deportesvario)
+    			<li>{{$deportesvario->id}}</li>
+    			{{$deportesvario->deporte}}
+    			{{$deportesvario->padre}}
+    			{{$deportesvario->descripcion}}
 
-    		@endforeach
+    		@endforeach-->
     	</ul>
     </div>
 </font>
